19ed2fc7cf5916c9f8fb9ab786098408e689cb01
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2022-11-01 13:56:56 +0300

    first12313215454

--- scripts/textToSpeech.js
+++ scripts/textToSpeech.js
@@ -7,9 +7,27 @@ Allbuttons.forEach((button) => {
     e.preventDefault();
     textButton = e.target.innerHTML;
     let textReading = new SpeechSynthesisUtterance(e.target.innerText);
+    voice = synth.getVoices();
+
+    for (let i = 0; i < voice.length; i++) {
+      if (voice[i].lang == "en-US") {
+        voice = synth.getVoices()[i];
+        textReading.voice = voice;
+        synth.speak(textReading);
+        console.log(voice);
+      }
+    }
+  });
+});
+
+/*Allbuttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    textButton = e.target.innerHTML;
+    let textReading = new SpeechSynthesisUtterance(e.target.innerText);
     voice = synth.getVoices()[10];
     voice.lang = "en-US";
     textReading.voice = voice;
     synth.speak(textReading);
   });
-});
+});  */
