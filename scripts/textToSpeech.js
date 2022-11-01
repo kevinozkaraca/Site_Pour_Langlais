9bb69652ecc7c1fea368153ed1cd8d9c49a1a488
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2022-11-01 16:47:30 +0300

    first12313215454

--- scripts/textToSpeech.js
+++ scripts/textToSpeech.js
@@ -1,26 +1,21 @@
 const Allbuttons = document.querySelectorAll("#textToSpeech");
 let synth = speechSynthesis,
   isSpeaking = true;
+let voice = synth.getVoices()[10];
 
 Allbuttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     e.preventDefault();
     textButton = e.target.innerHTML;
-    let textReading = new SpeechSynthesisUtterance(e.target.innerText);
-    voice = synth.getVoices();
-    console.log(voice);
-    voice = voice[10];
-    voice.default = voice[10];
+    let textReading = new SpeechSynthesisUtterance(textButton);
     voicelocalService = false;
-    voice["default"] = true;
     textReading.voice = voice;
     textReading.lang = "en-US";
-    textReading.text = e.target.innerText;
+    textReading.text = textButton;
     textReading.pitch = 1.0;
     textReading.rate = 1.0;
     textReading.volume = 1.0;
     textReading.voice = voice;
-    textReading.voice.default = true;
     textReading.voiceURI = "Google US English";
     console.log(textReading);
     synth.speak(textReading);
