44ca7000b2d2f4efe9d69b71f72d901d0e374cb4
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2022-11-19 00:56:19 +0300

    first12313215454

--- scripts/transformH1.js
+++ scripts/transformH1.js
@@ -1,7 +1,15 @@
-let h1Title = document.querySelectorAll("h1");
-// travail effectué sur les "div"
-h1Title.forEach((divs) => {
-  for (i = 0; i < divs.textContent.length; i++) {
-    divs.style.color = "blue";
-  }
-});
+// let h1Title = document.querySelectorAll("div");
+// let divForLetters = document.createElement("div");
+// let choiceForZindex = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+// // travail effectué sur les lettres
+// function transformH1() {
+//   h1Title.forEach((title) => {
+//     console.log(title.textContent.length);
+//     for (i = 0; (i = 11); i++) {
+//       divForLetters.innerHTML = title.textContent[i];
+//       divForLetters.className = "dlmfj";
+//       console.log(divForLetters);
+//     }
+//   });
+// }
+// transformH1();
