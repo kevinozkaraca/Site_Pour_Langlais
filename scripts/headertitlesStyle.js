aa03d12db95c13c071894e144dc879aeee41d4ea
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2022-12-04 09:24:50 +0300

    finalisation de l'unité 1

--- scripts/headertitlesStyle.js
+++ scripts/headertitlesStyle.js
@@ -2,6 +2,6 @@ let headertitles = document.querySelectorAll("a");
 
 headertitles.forEach(() => {
   for (let i = 0; i < 6; i++) {
-    headertitles[i].style.background = `rgb(${i * 0.5}, ${i * 10.5}, ${i * 30.5})`;
+    headertitles[i].style.background = `rgb(${i * 30.5}, ${i * 0.5}, ${i * 5.5})`;
   }
 });
--- scripts/pagestitlesStyle.js
+++ scripts/pagestitlesStyle.js
@@ -2,7 +2,7 @@ let pagestitles = document.querySelectorAll("a");
 
 pagestitles.forEach(() => {
   for (let i = 6; i < pagestitles.length; i++) {
-    pagestitles[i].style.background = `rgb(${i * 0.2}, ${i * 0.5}, ${i * 5})`;
+    pagestitles[i].style.background = `rgb(${i * 2.2}, ${i * 0.5}, ${i * 0.2})`;
   }
 });
 
